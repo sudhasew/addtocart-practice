c4d8aa2be80b16e2a889dbdd540a953fb37d407c
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-01-01 12:59:42 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// const cartItem = document.getElementById("addToCart");
+// const cartItem = document.getElementById("title");
+let addItemId = 0;
+function addToCart(item) {
+  addItemId += 1;
+  var selectedItem = document.createElement("div");
+  selectedItem.classList.add("cartImg");
+  selectedItem.setAttribute("id", addItemId);
+  var img = document.createElement("img");
+  img.setAttribute("src", item.children[0].currentSrc);
+  var title = document.createElement("div");
+  title.innerText = item.children[1].innerText;
+  var label = document.createElement("div");
+  label.innerText = item.children[2].children[0].innerText;
+  var select = document.createElement("span");
+  select.innerText = item.children[2].children[1].value;
+  label.append(select);
+  var cartItems = document.getElementById("title");
+  selectedItem.append(img);
+  selectedItem.append(title);
+  selectedItem.append(label);
+  cartItems.append(selectedItem);
+}
